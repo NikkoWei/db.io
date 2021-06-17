47aa02aa91b7bc5e7ee0c50ed003b608cca55ebf
weiliangning <wlnthousander@gmail.com>
2021-06-17 10:56:51 +0800

    test

---
+++ json/db.json2.js
@@ -0,0 +1,10 @@
+{items: [
+  {
+    // action: 'mdi-ticket',
+    items: [{ title: 'List Item' }],
+    title: '贷款账号',
+    subtitle: 'AcctNo',
+    datatype: 'CHAR',
+    detail: 'sdfsdfsdfd\ndsfdsfjk'
+  }]
+}
