5dd63701486f30ed114bf3c5c423372f4144d1cf
weiliangning <wlnthousander@gmail.com>
2021-06-17 11:24:01 +0800

    use data from .js

--- json/db.json.js
+++ json/db.json.js
@@ -1,11 +1,10 @@
-export default{
-    "items": [
-      {
-        "items": [{ "title": "List Item" }],
-        "title": "贷款账号",
-        "subtitle": "AcctNo",
-        "datatype": "CHAR",
-        "detail": "sdfsdfsdfd\ndsfdsfjk"
-      }
-    ]
-  }
+db={items: [
+  {
+    // action: 'mdi-ticket',
+    items: [{ title: 'List Item' }],
+    title: '贷款账号',
+    subtitle: 'AcctNo',
+    datatype: 'CHAR',
+    detail: 'sdfsdfsdfd\ndsfdsfjk'
+  }]
+}
